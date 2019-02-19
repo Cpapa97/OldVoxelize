51e47f911002eabe05726160ec0b374ba1fca070
Cpapa97 <cpapa97@gmail.com>
2019-02-19 15:38:46 -0600

    added example structure for methods

---
+++ python/VoxelStatistics.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+class VoxelStats:
+    @classmethod
+    def compute_something(self, voxels):
+        # Compute some statistic for a voxel structure
+
+        # Example method ^
+
+        raise NotImplementedError
